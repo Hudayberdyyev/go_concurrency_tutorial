6f0b348afead013d67fea8498e187718d1111263
arximed <hudayberdyyev.ahmet@gmail.com>
2022-08-04 18:54:13 +0500

    multiplexing approach

--- main.go
+++ main.go
@@ -9,20 +9,25 @@ import (
 func main() {
 	println("MAIN FUNCTION ENTERED")
 
-	ann := boring("ann")
-	jen := boring("jen")
-	for i := 0; i < 5; i++ {
-		fmt.Printf("You say: %q\n", <-ann)
-		fmt.Printf("You say: %q\n", <-jen)
+	c := fanIn(boring("ann"), boring("jen"))
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%q\n", <-c)
 	}
 	fmt.Println("You're boring; I'm leaving.")
 }
 
-func boring(msg string) chan string {
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() { for {c <- <-input1} }()
+	go func() { for {c <- <-input2} }()
+	return c
+}
+
+func boring(msg string) <-chan string {
 	c := make(chan string)
 	go func() {
 		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s %d", msg, i)
+			c <- fmt.Sprintf("%s: %d", msg, i)
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 	}()
