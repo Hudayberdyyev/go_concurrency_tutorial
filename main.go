4267e9cf844bfa1933a030d706fa2e06b155e993
arximed <hudayberdyyev.ahmet@gmail.com>
2022-08-04 19:06:30 +0500

    approach 3. restoring sequencing

--- main.go
+++ main.go
@@ -6,29 +6,42 @@ import (
 	"time"
 )
 
+type Message struct {
+	str  string
+	wait chan bool
+}
+
 func main() {
 	println("MAIN FUNCTION ENTERED")
 
 	c := fanIn(boring("ann"), boring("jen"))
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%q\n", <-c)
+		msg1 := <-c
+		fmt.Println(msg1.str)
+		msg2 := <-c
+		fmt.Println(msg2.str)
+
+		msg1.wait <- true
+		msg2.wait <- true
 	}
 	fmt.Println("You're boring; I'm leaving.")
 }
 
-func fanIn(input1, input2 <-chan string) <-chan string {
-	c := make(chan string)
+func fanIn(input1, input2 <-chan Message) <-chan Message {
+	c := make(chan Message)
 	go func() { for {c <- <-input1} }()
 	go func() { for {c <- <-input2} }()
 	return c
 }
 
-func boring(msg string) <-chan string {
-	c := make(chan string)
+func boring(msg string) <-chan Message {
+	c := make(chan Message)
+	waitForIt := make(chan bool)
 	go func() {
 		for i := 0; ; i++ {
-			c <- fmt.Sprintf("%s: %d", msg, i)
+			c <- Message{str: fmt.Sprintf("%s: %d", msg, i), wait: waitForIt}
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			<-waitForIt
 		}
 	}()
 	return c
