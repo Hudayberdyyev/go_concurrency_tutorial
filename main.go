1783baa9523688d9dd960dafa3b2a8a22f9e789b
arximed <hudayberdyyev.ahmet@gmail.com>
2022-08-04 18:24:32 +0500

    second approach (generator)

--- main.go
+++ main.go
@@ -8,17 +8,20 @@ import (
 
 func main() {
 	println("MAIN FUNCTION ENTERED")
-	c := make(chan string)
-	go boring("boring", c)
+	c := boring("boring")
 	for i := 0; i < 5; i++ {
 		fmt.Printf("You say: %q\n", <-c)
 	}
 	fmt.Println("You're boring; I'm leaving.")
 }
 
-func boring(msg string, c chan string) {
-	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+func boring(msg string) chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
 }
